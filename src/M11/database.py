760ae81bb8a02c462197f1e5c14ad6701bdfc588
sriharshagattu143 <24je0618@iitism.ac.in>
2026-03-17 23:31:43 +0530

    setup MongoDB client connection and environment loading

--- src/M11/database.py
+++ src/M11/database.py
@@ -2,8 +2,9 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env
+# Load environment variables from .env file
 load_dotenv()
+
 
 MONGO_URI = os.getenv("MONGO_URI")
 
